884d35c23b2b020a34d5b0c8dfc7b36b8a24e6fb
chernushov88@gmail.com <chernushov88@gmail.com>
2023-03-17 21:11:28 +0000

    Profile.vue v-2

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -2,11 +2,16 @@
   <q-page>
     <div class="profile-header full-width column flex-center">
       <div class="avatar-wrapper my-6 relative-position">
-        <q-img
-          class="w-48 border-4 rounded-full border-black-700"
-          src="~assets/logo.png"
-          :style="avatarStyle"
-        />
+        <q-img  src="~/assets/logo.png"
+                :style="avatarStyle"
+                class="w-48 border-4 rounded-full border-black-700"
+        >
+        </q-img>
+        <q-div
+          class="w-48 border-4 rounded-full border-black-700">
+          <img src="~/assets/logo.png" alt="" :style="avatarStyle">
+        </q-div>
+
         <q-btn
           icon="photo"
           rounded
@@ -21,30 +26,35 @@
     <div class="p-4 space-y-4">
       <q-card class="rounded-borders rounded-xl bg-grey-3 whitespace-pre-line">
         <q-card-section class="space-y-2">
-          <div class="text-h5 text-bold">Ваши данные</div>
+          <div class="text-h5 text-bold">Your details</div>
           <q-form class="space-y-2">
-            <q-input label="Имя" outlined />
-            <q-input label="Фамилия" outlined />
-            <q-input label="Город" outlined />
-            <q-btn label="Обновить" class="full-width bg-primary text-white" />
+            <q-input label="Name" outlined />
+            <q-input label="Surname" outlined />
+            <q-input label="City" outlined />
+            <q-btn label="Send" class="full-width bg-primary text-white" />
           </q-form>
         </q-card-section>
       </q-card>
       <q-card class="rounded-borders rounded-xl bg-grey-3 whitespace-pre-line">
         <q-card-section class="space-y-2">
-          <div class="text-h5 text-bold">Контактная информация</div>
+          <div class="text-h5 text-bold">Contact Information</div>
           <q-form class="space-y-2">
             <q-input
               v-model="formContact.phone"
-              mask="+7(###)###-##-##"
-              label="Телефон"
+              mask="(###)###-##-##"
+              label="Phone"
               unmasked-value
               outlined
             />
-            <q-btn label="Обновить" class="full-width bg-primary text-white" />
+            <q-btn label="Send" class="full-width bg-primary text-white" />
           </q-form>
         </q-card-section>
+        <q-card-section  class="p-10">
+          <div class="" v-for="i in 10" :key="i">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque beatae ipsam odit voluptatum. Accusantium aspernatur, ea inventore laudantium modi nihil nulla omnis perferendis, porro quae quisquam reiciendis repudiandae tempora?</div>
+        </q-card-section>
       </q-card>
+
+
     </div>
   </q-page>
 </template>
